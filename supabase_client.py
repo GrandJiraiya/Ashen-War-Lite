5758a00d07ab253d165a187424b1e4b8b5724508
Crash_Out_Crypto <86508100+GrandJiraiya@users.noreply.github.com>
2026-04-21 10:44:01 -0500

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 supabase: Client = create_client(
-    os.getenv("SUPABASE_URL"),
-    os.getenv("SUPABASE_KEY")
+    os.getenv("https://kscqzqryqkjjhtjhwxia.supabase.co"),
+    os.getenv("sb_publishable_0c4EgQus5EdJbwR1lWyH3w_rFl20v6m")
 )
 
 def get_supabase():
